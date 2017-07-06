1f01cc07fe38af8e413c8eb4b21376fdf08670cc
Matthew-Burfield <m1.burfield@gmail.com>
2017-07-06 16:35:03 +1000

    Fixed eslint errors to app now completes production build properly

--- src/components/Polls/Polls.css.js
+++ src/components/Polls/Polls.css.js
@@ -1,16 +1,16 @@
 export default {
   app: {
-    textAlign: 'center',
+    textAlign: 'center'
   },
   appLogo: {
     animation: 'appLogoSpin infinite 20s linear',
-    height: 80,
+    height: 80
   },
   appHeader: {
     backgroundColor: '#222',
     height: 150,
     padding: 20,
-    color: 'white',
+    color: 'white'
   },
   appIntro: {
     fontSize: 'large'
