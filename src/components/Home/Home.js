711bdcd06055ac9a90d847e4db39d8ba18bcf473
Matthew-Burfield <m1.burfield@gmail.com>
2017-07-06 11:39:35 +1000

    ejected from CRA and restructured files

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,19 +1,15 @@
 import React from 'react'
 import s from './Home.css'
 
-class Home extends React.Component {
-  render() {
-    return (
-      <div className={s.App}>
-        <div className={s.AppHeader}>
-          <h2>Welcome to React</h2>
-        </div>
-        <p className={s.AppIntro}>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    )
-  }
-}
+const Home = () => (
+  <div style={s.app}>
+    <div style={s.appHeader}>
+      <h2>Welcome to React</h2>
+    </div>
+    <p style={s.appIntro}>
+      To get started, edit <code>src/App.js</code> and save to reload.
+    </p>
+  </div>
+)
 
 export default Home
